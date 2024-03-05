95e6e448945870c4a332c9621a95bcc0ab7183f9
bahriilhame <bahri.ilhame@gmail.com>
2024-03-05 23:28:01 +0100

    added the mobile navbar (menu)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
+import {close,logo,menu} from '../assets'
 import { navLinks } from '../constants'
-import { logo } from '../assets'
 
 const Navbar = () => {
+    const [toggle,setToggle]=useState(false)
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
+        {/* Desktop Menu Starts */}
         <img src={logo} alt="logo" className='w-[124px] h-[32px]'/>
         <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
             {navLinks.map((nav,index)=>{
@@ -12,6 +15,24 @@ const Navbar = () => {
                 </li>
             })}
         </ul>
+        {/* Desktop Menu Ends */}
+
+        {/* Mobile Menu Starts */}
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain'  onClick={()=>setToggle((prev)=>!prev)}/>
+
+            <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                <ul className='list-none flex flex-col justify-end items-center flex-1'>
+                {navLinks.map((nav,index)=>{
+                    return <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1 ? 'mr-0' : 'mb-4'} text-white`} >
+                        <a href={`#${nav.id}`}>{nav.title}</a>
+                    </li>
+                })}
+                </ul>
+            </div>
+        </div>
+        {/* Mobile Menu Ends */}
+
     </nav>
   )
 }
