712f59bceaaa45a04999e8a0b5371d29ae25522c
bahriilhame <bahri.ilhame@gmail.com>
2024-03-05 23:20:31 +0100

    added the desktop navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { navLinks } from '../constants'
+import { logo } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+        <img src={logo} alt="logo" className='w-[124px] h-[32px]'/>
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+            {navLinks.map((nav,index)=>{
+                return <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index===navLinks.length-1 ? 'mr-0' : 'mr-10'} text-white`} >
+                    <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+            })}
+        </ul>
+    </nav>
   )
 }
 
